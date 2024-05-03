beb4907d5b3ec42d1450e824755c9ce307401ee9
jabbar84o <167693370+jabbar84o@users.noreply.github.com>
2024-05-03 20:47:43 +0500

    Add files via upload

---
+++ task 12/main.ts
@@ -0,0 +1,9 @@
+// Start with the array you used in Exercise 11, but instead of just printing each person’s name, 
+// print a message to them. The text of each message should be the same, but each message should be 
+// personalized with the person’s name.
+let names :string [] = ["Jabbar","Qadeer","Akash","Riaz",];
+let message : string = "Do yo like to play football"
+console.log(names[0] + " " + message)
+console.log(names[1] + " " + message)
+console.log(names[2] + " " + message)
+console.log(names[3] + " " + message)
